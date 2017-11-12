c8b50e46f83fdba0eee07d3114d3e105ded4a5a6
Claudia Ruiz <ruclaudia@gmail.com>
2017-11-12 20:05:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+//Scooter needs a year, color, and model to get a pickup location
+function Scooter (year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+ function Driver (name, age, experience) {
+   this.name = name
+   this.age = age
+   this.experience = experience
+ }
+
+function PickupLocation (address, city) {
+  this.address = address
+  this.city = city
+}
