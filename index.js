9e814022bb289ac1f30059f098a347a505999b3c
James Gomis <jamesislike@gmail.com>
2017-11-12 20:10:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function Scooter (year, color, model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+//let harley = new Scooter(2001,'yello','davidson')
+
+function Driver(name, age, experience){
+  this.name = name
+  this.experience = experience
+  this.age = age
+
+}
+
+//let john = new driver('john', 29, '10 years')
+
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city
+}
+
+//let NYC = new PickupLocation('100 W 47th St', 'New York')
