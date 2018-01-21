59cf4205c51d2f0a08bd3538ca50839077f70d9f
JSpeed8th <jspeed8th@gmail.com>
2018-01-21 22:47:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-//Scooter with `year`, `color`, and `model` properties
+/*Create a constructor function for each of the following:
++ Scooter with `year`, `color`, and `model` properties
++ Driver with `name`, `age`, and `experience` properties
++ PickupLocation with `address` and `city` properties*/
+
 function Scooter (year, color, model) {
   this.year = year
   this.color = color
   this.model = model
 }
-
-//Driver with `name`, `age`, and `experience` properties
 function Driver (name, age, experience) {
   this.name = name
   this.age = age
   this.experience = experience
 }
-
-//PickupLocation with `address` and `city` properties
-function PickupLocation (address, city) {
-  this['address'] = address
-  this['city'] = city
+function PickupLocation (address, city, properties) {
+  this.address = address
+  this.city = city
+  this.properties = properties
 }
